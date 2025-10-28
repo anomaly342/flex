d5b2103ea2a1bbb37c0e3a5cff5bb65ea18944e3
vvvvvvvvv <152473909+cs6630463@users.noreply.github.com>
2025-10-29 02:43:24 +0700

    add zone-details page

---
+++ client/app/(main)/individual/details/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+// import Navbar from "../_navbar/page";
+
+export const metadata: Metadata = {
+    title: "Room Booking",
+};
+
+export default function ProfileLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            {/* <Navbar /> */}
+            <main>{children}</main>
+        </>
+    );
+}
