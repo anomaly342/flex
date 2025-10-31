e2734d68d3217a99570b8939a3b91f38190f1b68
prosper <s6604062630463@email.kmutnb.ac.th>
2025-10-31 11:46:54 +0700

    fix some css

--- client/app/(main)/orders/page.tsx
+++ client/app/(main)/orders/page.tsx
@@ -1,58 +1,50 @@
 "use client";
-import Image from "next/image";
 import "./orders.css";
 
-export default function Home() {
-    let role = "Membership";
-    let points = 999;
-
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        console.log("Button clicked!", event);
-    };
-
+export default function OrderHistory() {
     return (
         <div className="maindiv">
             <main>
-                <div className="flex justify-center">
-                    <p>Order History</p>
+                <div className="header-div">
+                    <h1>Order History</h1>
                 </div>
-                <div className="block pb-3 pl-3 pr-3">
-                    <h1>Today</h1>
-                    <div className="border-1 flex flex-row justify-start p-3 space-x-5">
-                        <div className="text-center">
+                <div className="reminder-section">
+                    <h2>Today</h2>
+                    <div className="reminder-detail">
+                        <div className="text-time">
                             <p>08:00</p>
                             <p>to</p>
                             <p>12:00</p>
                         </div>
-                        <div className="text-start flex flex-col justify-center">
+                        <div className="text-location">
                             <p>Room 1</p>
                             <p>(Room 1 Details)</p>
                         </div>
                     </div>
                 </div>
-                <div className="block pb-3 pl-3 pr-3">
-                    <h1>Friday</h1>
-                    <div className="border-1 flex flex-row justify-start p-3 space-x-5">
-                        <div className="text-center">
+                <div className="reminder-section">
+                    <h2>Friday</h2>
+                    <div className="reminder-detail">
+                        <div className="text-time">
                             <p>13:00</p>
                             <p>to</p>
                             <p>16:00</p>
                         </div>
-                        <div className="text-start flex flex-col justify-center">
+                        <div className="text-location">
                             <p>Room 5</p>
                             <p>(Room 5 Details)</p>
                         </div>
                     </div>
                 </div>
-                <div className="block pb-3 pl-3 pr-3">
-                    <h1>Sunday</h1>
-                    <div className="border-1 flex flex-row justify-start p-3 space-x-5">
-                        <div className="text-center">
+                <div className="reminder-section">
+                    <h2>Sunday</h2>
+                    <div className="reminder-detail">
+                        <div className="text-time">
                             <p>09:00</p>
                             <p>to</p>
                             <p>12:00</p>
                         </div>
-                        <div className="text-start flex flex-col justify-center">
+                        <div className="text-location">
                             <p>Room 8</p>
                             <p>(Room 8 Details)</p>
                         </div>
